8189db0b79e55673145466afac133047bef9f7ed
Brandon Carter <bcarteratx@gmail.com>
2020-03-18 10:30:54 -0500

    create superuser

--- main_app/views.py
+++ main_app/views.py
@@ -4,19 +4,6 @@ from .models import Guitar
 
 # Create your views here.
 
-class Guitar:
-  def __init__(self, model, brand, description, year):
-    self.model = model
-    self.brand = brand
-    self.description = description
-    self.year = year
-
-guitars = [
-  Guitar('Telecaster', 'Fender', 'Sunburst Deluxe with wide range humbuckers', 1973),
-  Guitar('Jazzmaster', 'Fender', 'Olympic White with tortoise shell pickguard', 2010),
-  Guitar('Casino', 'Epiphone', 'USA made reissue', 2020)
-]   
-
 def home(request):
   return HttpResponse('<h1>Hello Guitarists</h1>')
 
@@ -24,5 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def guitars_index(request):
-#  guitars = Guitar.objects.all()
+  guitars = Guitar.objects.all()
   return render(request, 'guitars/index.html', { 'guitars': guitars })
